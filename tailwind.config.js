d98b034e6bb3bebb751e3e63c6ee5143dc03067b
Giacomo Salici <giambo2000@gmail.com>
2023-06-23 01:11:11 +0200

    flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,15 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   plugins: [
     require('@tailwindcss/typography'),
-    require("daisyui")], //Make sure you require daisyui AFTER @tailwindcss/typography
-  daisyui: {
+    //require("daisyui"), //Make sure you require daisyui AFTER @tailwindcss/typography
+    require('flowbite/plugin')
+  ],
+    daisyui: {
     themes: ["cupcake"],
   },
 }
