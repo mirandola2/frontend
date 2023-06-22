fab4b0be87faa15e0e8ef05540d716eb6e77ee9d
Giacomo Salici <giambo2000@gmail.com>
2023-06-23 01:11:11 +0200

    flowbite

---
+++ src/components/Menu.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Navbar, NavbarLogo, NavbarCollapse, NavbarLink } from 'flowbite-vue'
+import { Dropdown, ListGroup, ListGroupItem } from 'flowbite-vue'
+
+</script>
+<template>
+    <Navbar>
+      <template #logo>
+        <NavbarLogo link="/" alt="logo" image-url="logo_colored.png">
+          Agesci Mirandola 2
+        </NavbarLogo>
+      </template>
+      <template #default="{isShowMenu}">
+        <NavbarCollapse :isShowMenu="isShowMenu">
+          <NavbarLink isActive link="#">Home</NavbarLink>
+          <NavbarLink link="#">L'Associazione</NavbarLink>
+          <NavbarLink link="#">Le Branche</NavbarLink>
+          <NavbarLink link="#">Contatti</NavbarLink>
+        </NavbarCollapse>
+      </template>
+    </Navbar>
+</template>
