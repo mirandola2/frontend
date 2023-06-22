550ad07137a880fb57ca26b8e3027ba6f372a5c7
Giacomo Salici <giambo2000@gmail.com>
2023-06-23 00:06:24 +0200

    init vue

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# frontend
-Frontend del sito web del Gruppo Agesci Mirandola 2. Built with Vue and Tailwind.
+# Sito web del Gruppo Scout Mirandola 2
+
+Frontend del sito web del Gruppo Agesci Mirandola 2. Built with Vue 3 + Vite and Tailwind.
---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script setup>
+</script>
+
+<template>
+  
+
+  <div class="p-5">
+
+    <div class="navbar bg-base-100 shadow-xl rounded-xl">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <label tabindex="0" class="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+          <ul class="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+    <p class="normal-case text-xl">Gruppo Scout Mirandola 2</p>
+  </div>
+  <div class="navbar-center hidden lg:flex">
+    <ul class="menu menu-horizontal px-1">
+      <li><a>Item 1</a></li>
+      <li tabindex="0">
+        <details>
+          <summary>Parent</summary>
+          <ul class="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Item 3</a></li>
+    </ul>
+  </div>
+  <div class="navbar-end">
+    <a class="btn">Button</a>
+  </div>
+</div>
+
+    <h1 class="text-3xl">
+    Hello world!
+    </h1>
+    <button class="btn">Button</button>
+
+  </div>
+  
+</template>
+
+
