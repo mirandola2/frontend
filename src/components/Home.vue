685857fedbb39a73bdc4d90a6313dba9547e5fe3
Giacomo Salici <giambo2000@gmail.com>
2023-06-23 18:14:43 +0200

    bug slash

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
     <div class="hero-overlay bg-primary bg-opacity-50 rounded-xl"></div>
     <div class="hero-content p-20 text-center text-white">
       <div class="max-w-md">
-        <img class="w-52 inline p-3 pb-8" src="logo_white.svg" />
+        <img class="w-52 inline p-3 pb-8" src="/logo_white.svg" />
         <h1 class="mb-5 text-2xl md:text-4xl font-bold">
           Un'avventura giocata insieme da 40 anni!
         </h1>
