0e7a2d30873d5e8e1e2baa8cb85c8ceb67cd2a0a
Giacomo Salici <giambo2000@gmail.com>
2023-06-23 13:24:44 +0200

    navbar, mobile color

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,24 @@ import Menu from "./components/Menu.vue";
 <template>
   <div>
     <Menu />
-
+    <div class="container mx-auto p-2">
     <div
-      class="hero"
+      class="hero rounded-xl"
       style="
         background-image: url('hero.jpg');
       "
       
     >
-      <div class="hero-overlay bg-primary bg-opacity-50"></div>
+      <div class="hero-overlay bg-primary bg-opacity-50 rounded-xl"></div>
       <div class="hero-content p-20 text-center text-white">
         <div class="max-w-md">
           <img class="w-52 inline p-3 pb-8" src="logo_white.svg" />
-          <h1 class="mb-5 text-4xl font-bold">
+          <h1 class="mb-5 text-2xl md:text-4xl font-bold">
             Un'avventura giocata insieme da 40 anni!
           </h1>
         </div>
       </div>
     </div>
+  </div>
   </div>
 </template>
