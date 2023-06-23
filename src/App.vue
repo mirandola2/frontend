ba3d232d4b1390e4cb581991e645a9a5ac5e7829
Giacomo Salici <giambo2000@gmail.com>
2023-06-23 16:21:54 +0200

    routes!

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <script setup>
-import Menu from "./components/Menu.vue";
+import NavBar from "./components/partials/NavBar.vue";
+
+
 </script>
 
 <template>
   <div>
-    <Menu />
+    <NavBar />
     <div class="container mx-auto p-2">
-    <div
-      class="hero rounded-xl"
-      style="
-        background-image: url('hero.jpg');
-      "
-      
-    >
-      <div class="hero-overlay bg-primary bg-opacity-50 rounded-xl"></div>
-      <div class="hero-content p-20 text-center text-white">
-        <div class="max-w-md">
-          <img class="w-52 inline p-3 pb-8" src="logo_white.svg" />
-          <h1 class="mb-5 text-2xl md:text-4xl font-bold">
-            Un'avventura giocata insieme da 40 anni!
-          </h1>
-        </div>
-      </div>
-    </div>
+
+    <router-view></router-view>
   </div>
   </div>
 </template>
