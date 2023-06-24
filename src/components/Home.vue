f3fe5a9a607171aca48e09518d513f034220201a
Giacomo Salici <giambo2000@gmail.com>
2023-06-24 16:55:36 +0200

    hero template

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,7 @@
 <template>
-  <div class="hero rounded-xl" style="background-image: url('hero.jpg')">
-    <div class="hero-overlay bg-primary bg-opacity-50 rounded-xl"></div>
-    <div class="hero-content p-20 text-center text-white">
-      <div class="max-w-md">
-        <img class="w-52 inline p-3 pb-8" src="/logo_white.svg" />
-        <h1 class="mb-5 text-2xl md:text-4xl font-bold">
-          Un'avventura giocata insieme da 40 anni!
-        </h1>
-      </div>
-    </div>
-  </div>
+  <Hero title="Un'avventura giocata insieme da 40 anni!" image="/hero.jpg" color="bg-primary" logo="/logo_white.svg"/>
 </template>
-<script>
-export default{
-    name: 'Home',
-}
+<script setup>
+import Hero from './partials/Hero.vue';
+
 </script>
---
+++ src/components/partials/Hero.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps({title: {
+    type: String,
+    required: true
+}, image: {
+    type: String,
+    default: "/hero.jpg",
+}, color: {
+    type: String,
+    default: "bg-primary",
+}, logo: {
+    type: String,
+    default: undefined,
+}});
+
+</script>
+
+<template>
+  <div class="hero rounded-xl" :style="{'background-image': 'url(' + image + ')'}">
+    <div class="hero-overlay bg-opacity-50 rounded-xl" :class="color"></div>
+    <div class="hero-content p-20 text-center text-white">
+      <div class="max-w-md"> 
+        <img v-if="logo != undefined" :src="logo" class="w-52 inline p-3 pb-8" />
+        <h1 class="mb-5 text-2xl md:text-4xl font-bold">
+          {{ title }}
+        </h1>
+      </div>
+    </div>
+  </div>
+
+
+</template>
+
+
