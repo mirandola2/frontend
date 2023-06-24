8763c270557b30781f0610c88805e6627a952752
Giacomo Salici <giambo2000@gmail.com>
2023-06-24 20:54:33 +0200

    update

--- src/components/partials/Hero.vue
+++ src/components/partials/Hero.vue
@@ -18,10 +18,10 @@ const props = defineProps({title: {
 <template>
   <div class="hero rounded-xl" :style="{'background-image': 'url(' + image + ')'}">
     <div class="hero-overlay bg-opacity-60 rounded-xl" :class="'bg-' + color"></div>
-    <div class="hero-content py-20 text-center w-full" :class="'text-' + color + '-content'">
+    <div class="hero-content py-20 text-center w-4/5 text-white">
     <div>
         <img v-if="logo != undefined" :src="logo" class="w-52 inline p-3 pb-8" />
-        <h1 class="mb-5 text-2xl md:text-3xl lg:text-4xl font-bold">
+        <h1 class="mb-5 text-3xl md:text-4xl lg:text-5xl font-display">
           {{ title }}
         </h1>
     </div>
