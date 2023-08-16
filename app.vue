835cadb64312d8ca954fe4decca1782ce0612ca4
Giacomo Salici <giambo2000@gmail.com>
2023-08-16 18:35:33 +0200

    menu desktop

--- app.vue
+++ app.vue
@@ -1,14 +1,120 @@
 <script setup>
+import { ref } from "vue";
+
+const menu = ref({
+  Home: "/",
+  "Chi Siamo": {
+    "L'Associazione": "/associazione",
+    "Il Gruppo": "/gruppo",
+    "La Comunità Capi": "/coca",
+  },
+  "Lupetti e Coccinelle": {
+    "La Branca L/C": "/lc/",
+    "Branco della Roccia Azzurra": "/lc/lupetti",
+    "Cerchio del Bosco Gioioso": "/lc/coccinelle",
+    "Piccole Orme": "/lc/piccole-orme",
+    Specialità: "/lc/specialita",
+  },
+  "Esploratori e Guide": {
+    "La Branca E/G": "/eg",
+    "Reparto Brownsea": "/eg/brownsea",
+    "Reparto Antares": "/eg/antares",
+    "Campi di Specialità e Competenza": "/eg/campetti",
+    "Specialità e Brevetti": "/eg/specialità-brevetti",
+  },
+  "Rover e Scolte": {
+    "La Branca R/S": "/rs/",
+    "Clan Fuoco Madonna della Valle": "/rs/mdv",
+    Noviziato: "/rs/noviziato",
+    "Epppi/Ross": "/rs/epppi-ross",
+  },
+  Archivio: {
+    Totem: "/totem",
+    "Staff del Passato": "/albo",
+    "Campi e Route": "/campi",
+  },
+  Utilities: {
+    "Ricerca Codice Socio": "/codice",
+    "Uniforme e Distintivi": "/uniforme",
+  },
+});
 </script>
 
 <template>
-  <div class="mx-auto flex flex-col min-h-screen">
-    
-    <NavBar />
+  <div class="drawer drawer-end" tabindex="0">
+    <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col">
+      <!-- Navbar -->
+      <div class="navbar bg-primary text-primary-content lg:sticky lg:top-0 lg:z-10">
+        <div class="container mx-auto px-2">
+          <div class="flex-1 w-3/4 flex">
+            <h1 class="text-2xl my-auto font-bold">
+              <a href="/">Mirandola 2</a>
+            </h1>
+          </div>
 
-    <NuxtPage />
+          <div class="navbar-center"></div>
 
+          <div class="flex-none hidden lg:block">
+            <ul class="menu menu-horizontal">
+              <!-- Navbar menu content here -->
+              <template v-for="(submenu, mainItem) in menu" :key="mainItem">
+              <li>
+                <!-- Check if the submenu is an object -->
+                <template v-if="typeof submenu === 'object'">
+                  <div class="menu-dropdown-toggle dropdown dropdown-end dropdown-bottom dropdown-hover hover:text-secondary">
+                    <label tabindex="0" class="">{{ mainItem }}</label>
+                    <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-secondary text-white rounded-box w-52">
+                      <!-- Loop through the sub-menu items -->
+                      <li v-for="(link, subItem) in submenu" :key="subItem">
+                        <a :href="link">{{ subItem }}</a>
+                      </li>
+                    </ul>
+                  </div>
+                </template>
+                <!-- If not an object, it's a direct link -->
+                <template v-else>
+                  <a :href="submenu" class="hover:text-secondary">{{ mainItem }}</a>
+                </template>
+              </li>
+            </template>
+            </ul>
+          </div>
+        </div>
 
-    <Footer />    
+        <div class="flex-none lg:hidden">
+          <label for="my-drawer-3" class="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-6 h-6 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </label>
+        </div>
+      </div>
+      <!-- Page content here -->
+
+      <div class="mx-auto flex flex-col min-h-screen">
+        <NuxtPage />
+
+        <Footer />
+      </div>
+    </div>
+    <div class="drawer-side">
+      <label for="my-drawer-3" class="drawer-overlay"></label>
+      <ul class="menu p-4 w-80 h-full bg-secondary text-neutral">
+        <!-- Sidebar content here -->
+        <li><a>Sidebar Item 1</a></li>
+        <li><a>Sidebar Item 2</a></li>
+      </ul>
+    </div>
   </div>
 </template>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,7 +1,46 @@
-<script>
-export default {
-  name: "NavBar",
-};
+<script setup>
+import { ref } from 'vue'
+
+
+
+const menu = ref({
+  "Home": "/",
+  "Chi Siamo": {
+    "L'Associazione": "/associazione",
+    "Il Gruppo": "/gruppo",
+    "La Comunità Capi": "/coca"
+  },
+  "Lupetti e Coccinelle": {
+    "La Branca L/C": "/lc/",
+    "Branco della Roccia Azzurra": "/lc/lupetti",
+    "Cerchio del Bosco Gioioso": "/lc/coccinelle",
+    "Piccole Orme": "/lc/piccole-orme",
+    "Specialità": "/lc/specialita"
+  },
+  "Esploratori e Guide": {
+    "La Branca E/G": "/eg",
+    "Reparto Brownsea": "/eg/brownsea",
+    "Reparto Antares": "/eg/antares",
+    "Campi di Specialità e Competenza": "/eg/campetti",
+    "Specialità e Brevetti": "/eg/specialità-brevetti"
+  },
+  "Rover e Scolte": {
+    "La Branca R/S": "/rs/",
+    "Clan Fuoco Madonna della Valle": "/rs/mdv",
+    "Noviziato": "/rs/noviziato",
+    "Epppi/Ross": "/rs/epppi-ross"
+  },
+  "Archivio": {
+    "Totem": "/totem",
+    "Staff del Passato": "/albo",
+    "Campi e Route": "/campi"
+  },
+  "Utilities": {
+    "Ricerca Codice Socio": "/codice",
+    "Uniforme e Distintivi": "/uniforme"
+  }
+})
+
 </script>
 <template>
   <div class="navbar bg-primary text-primary-content sticky top-0 z-10">
@@ -15,106 +54,39 @@ export default {
       <div class="navbar-end w-10 ml-auto">
         <div class="dropdown dropdown-end">
           <label tabindex="0" class="btn btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              class="inline-block w-6 h-6 stroke-current"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
             </svg>
           </label>
 
-          <ul
-            tabindex="0"
-            class="menu menu-xl dropdown-content mt-3 mr-2 z-[1] rounded-box bg-secondary w-72"
-          >
-            <li><a href="/">Home</a></li>
-
-            <li tabindex="1">
-              <details>
-                <summary>Chi siamo</summary>
-
-                <ul class="">
-                  <li><a href="/associazione">L'Associazione</a></li>
-                  <li><a href="/gruppo">Il Gruppo</a></li>
-                  <li><a href="/coca">La Comunità Capi</a></li>
-                </ul>
-              </details>
-            </li>
-
-            <li tabindex="1">
-              <details>
-                <summary>Lupetti e Coccinelle</summary>
-
-                <ul class="">
-                  <li><a href="/lc/">La Branca L/C</a></li>
-                  <li><a href="/lc/lupetti">Branco della Roccia Azzurra</a></li>
-                  <li>
-                    <a href="/lc/coccinelle">Cerchio del Bosco Gioioso</a>
-                  </li>
-                  <li><a href="/lc/piccole-orme">Piccole Orme</a></li>
-                  <li><a href="/lc/specialita">Specialità</a></li>
-                </ul>
-              </details>
-            </li>
-
-            <li tabindex="2">
-              <details>
-                <summary>Esploratori e Guide</summary>
-                <ul class="">
-                  <li><a href="/eg">La Branca E/G</a></li>
-                  <li><a href="/eg/brownsea">Reparto Brownsea</a></li>
-                  <li><a href="/eg/antares">Reparto Antares</a></li>
-                  <li>
-                    <a href="/eg/campetti">Campi di Specialità e Competenza</a>
-                  </li>
-                  <li>
-                    <a href="/eg/specialità-brevetti">Specialità e Brevetti</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li tabindex="3">
-              <details>
-                <summary>Rover e Scolte</summary>
-                <ul class="">
-                  <li><a href="/rs/">La Branca R/S</a></li>
-                  <li><a href="/rs/mdv">Clan Fuoco Madonna della Valle</a></li>
-                  <li><a href="/rs/noviziato">Noviziato</a></li>
-                  <li><a href="/rs/epppi-ross">Epppi/Ross</a></li>
-                </ul>
-              </details>
-            </li>
-
-            <li tabindex="4">
-              <details>
-                <summary>Archivio</summary>
-                <ul class="">
-                  <li><a href="/totem">Totem</a></li>
-                  <li><a href="/albo">Staff del Passato</a></li>
-                  <li><a href="/campi">Campi e Route</a></li>
-                </ul>
-              </details>
-            </li>
-
-            <li tabindex="5">
-              <details>
-                <summary>Utilities</summary>
-                <ul class="">
-                  <li><a href="/codice">Ricerca Codice Socio</a></li>
-                  <li><a href="/uniforme">Uniforme e Distintivi</a></li>
-                </ul>
-              </details>
-            </li>
+          <ul tabindex="0" class="menu menu-xl dropdown-content mt-3 mr-2 z-[1] rounded-box bg-secondary w-72">
+            <!-- Loop through the menu dictionary -->
+            <template v-for="(submenu, mainItem) in menu" :key="mainItem">
+              <li>
+                <!-- Check if the submenu is an object -->
+                <template v-if="typeof submenu === 'object'">
+                  <details>
+                    <summary>{{ mainItem }}</summary>
+                    <ul class="">
+                      <!-- Loop through the sub-menu items -->
+                      <li v-for="(link, subItem) in submenu" :key="subItem">
+                        <a :href="link">{{ subItem }}</a>
+                      </li>
+                    </ul>
+                  </details>
+                </template>
+                <!-- If not an object, it's a direct link -->
+                <template v-else>
+                  <a :href="submenu">{{ mainItem }}</a>
+                </template>
+              </li>
+            </template>
           </ul>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+
+
