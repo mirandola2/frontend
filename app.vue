7269c9a8f6da62b43ed79e328488303357ffb35d
Giacomo Salici <giambo2000@gmail.com>
2023-08-16 19:05:48 +0200

    resposinve navbar

--- app.vue
+++ app.vue
@@ -2,7 +2,6 @@
 import { ref } from "vue";
 
 const menu = ref({
-  Home: "/",
   "Chi Siamo": {
     "L'Associazione": "/associazione",
     "Il Gruppo": "/gruppo",
@@ -13,7 +12,7 @@ const menu = ref({
     "Branco della Roccia Azzurra": "/lc/lupetti",
     "Cerchio del Bosco Gioioso": "/lc/coccinelle",
     "Piccole Orme": "/lc/piccole-orme",
-    Specialità: "/lc/specialita",
+    "Specialità": "/lc/specialita",
   },
   "Esploratori e Guide": {
     "La Branca E/G": "/eg",
@@ -25,11 +24,11 @@ const menu = ref({
   "Rover e Scolte": {
     "La Branca R/S": "/rs/",
     "Clan Fuoco Madonna della Valle": "/rs/mdv",
-    Noviziato: "/rs/noviziato",
+    "Noviziato": "/rs/noviziato",
     "Epppi/Ross": "/rs/epppi-ross",
   },
   Archivio: {
-    Totem: "/totem",
+    "Totem": "/totem",
     "Staff del Passato": "/albo",
     "Campi e Route": "/campi",
   },
@@ -59,43 +58,33 @@ const menu = ref({
             <ul class="menu menu-horizontal">
               <!-- Navbar menu content here -->
               <template v-for="(submenu, mainItem) in menu" :key="mainItem">
-              <li>
-                <!-- Check if the submenu is an object -->
-                <template v-if="typeof submenu === 'object'">
-                  <div class="menu-dropdown-toggle dropdown dropdown-end dropdown-bottom dropdown-hover hover:text-secondary">
-                    <label tabindex="0" class="">{{ mainItem }}</label>
-                    <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-secondary text-white rounded-box w-52">
-                      <!-- Loop through the sub-menu items -->
-                      <li v-for="(link, subItem) in submenu" :key="subItem">
-                        <a :href="link">{{ subItem }}</a>
-                      </li>
-                    </ul>
-                  </div>
-                </template>
-                <!-- If not an object, it's a direct link -->
-                <template v-else>
-                  <a :href="submenu" class="hover:text-secondary">{{ mainItem }}</a>
-                </template>
-              </li>
-            </template>
+                <li>
+                  <!-- Check if the submenu is an object -->
+                  <template v-if="typeof submenu === 'object'">
+                    <div class="menu-dropdown-toggle dropdown dropdown-end dropdown-bottom dropdown-hover hover:text-secondary">
+                      <label tabindex="0" class="">{{ mainItem }}</label>
+                      <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-secondary text-white rounded-box">
+                        <!-- Loop through the sub-menu items -->
+                        <li v-for="(link, subItem) in submenu" :key="subItem">
+                          <a :href="link">{{ subItem }}</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </template>
+                  <!-- If not an object, it's a direct link -->
+                  <template v-else>
+                    <a :href="submenu" class="hover:text-secondary">{{ mainItem }}</a>
+                  </template>
+                </li>
+              </template>
             </ul>
           </div>
         </div>
 
         <div class="flex-none lg:hidden">
           <label for="my-drawer-3" class="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              class="inline-block w-6 h-6 stroke-current"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
             </svg>
           </label>
         </div>
@@ -108,13 +97,46 @@ const menu = ref({
         <Footer />
       </div>
     </div>
-    <div class="drawer-side">
+    <div class="drawer-side min-h-full overflow-y-scroll ">
       <label for="my-drawer-3" class="drawer-overlay"></label>
-      <ul class="menu p-4 w-80 h-full bg-secondary text-neutral">
-        <!-- Sidebar content here -->
-        <li><a>Sidebar Item 1</a></li>
-        <li><a>Sidebar Item 2</a></li>
-      </ul>
+
+
+      <div class="flex flex-row min-h-full ">
+        <div class="bg-[#07ac0f] w-1"></div>
+        <div class="bg-[#ffee18] w-1"></div>
+       
+
+
+        <div>
+          <ul class="menu p-4 w-80 bg-secondary min-h-full overflow-y-scroll  text-white">
+            <!-- Sidebar content here -->
+            <img src="/logo_white.svg" class="h-24 mb-8 " alt="">
+            <li> <a href="/">Home</a></li>
+            <template v-for="(submenu, mainItem) in menu" :key="mainItem">
+              <!-- Check if the submenu is an object -->
+              <template v-if="typeof submenu === 'object'">
+                <li>
+                  <details>
+                    <summary>{{ mainItem }}</summary>
+                    <ul>
+                      <!-- Loop through the sub-menu items -->
+                      <li v-for="(link, subItem) in submenu" :key="subItem">
+                        <a :href="link">{{ subItem }}</a>
+                      </li>
+                    </ul>
+                  </details>
+                </li>
+              </template>
+              <!-- If not an object, it's a direct link -->
+              <template v-else>
+                <li><a :href="submenu" class="hover:text-secondary">{{ mainItem }}</a></li>
+              </template>
+
+            </template>
+          </ul>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
