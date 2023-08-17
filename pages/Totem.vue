c1cd59e8fd8ada744453954e7d5954aa918e380c
Giacomo Salici <giambo2000@gmail.com>
2023-08-17 13:01:26 +0200

    totems query

--- pages/Totem.vue
+++ pages/Totem.vue
@@ -1,166 +1,88 @@
 <template>
-    <div class="overflow-x-auto">
-  <table class="table table-lg table-pin-rows rounded-rows">
-  <thead>
-    <tr>
-      <th>A</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>Ant-Man</td></tr>
-    <tr><td>Aquaman</td></tr>
-    <tr><td>Asterix</td></tr>
-    <tr><td>The Atom</td></tr>
-    <tr><td>The Avengers</td></tr>
-  </tbody>
-  <thead>
-    <tr>
-      <th>B</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>Batgirl</td></tr>
-    <tr><td>Batman</td></tr>
-    <tr><td>Batwoman</td></tr>
-    <tr><td>Black Canary</td></tr>
-    <tr><td>Black Panther</td></tr>
-  </tbody>
-  <thead>
-    <tr>
-      <th>C</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>Captain America</td></tr>
-    <tr><td>Captain Marvel</td></tr>
-    <tr><td>Catwoman</td></tr>
-    <tr><td>Conan the Barbarian</td></tr>
-  </tbody>
-  <thead>
-    <tr>
-      <th>D</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>Daredevil</td></tr>
-    <tr><td>The Defenders</td></tr>
-    <tr><td>Doc Savage</td></tr>
-    <tr><td>Doctor Strange</td></tr>
-  </tbody>
-  <thead>
-    <tr>
-      <th>E</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>Elektra</td></tr>
-  </tbody>
-  <thead>
-    <tr>
-      <th>F</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>Fantastic Four</td></tr>
-  </tbody>
-  <thead>
-    <tr>
-      <th>G</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>Ghost Rider</td></tr>
-    <tr><td>Green Arrow</td></tr>
-    <tr><td>Green Lantern</td></tr>
-    <tr><td>Guardians of the Galaxy</td></tr>
-  </tbody>
-  <thead>
-    <tr>
-      <th>H</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>Hawkeye</td></tr>
-    <tr><td>Hellboy</td></tr>
-    <tr><td>Incredible Hulk</td></tr>
-  </tbody>
-  <thead>
-    <tr>
-      <th>I</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>Iron Fist</td></tr>
-    <tr><td>Iron Man</td></tr>
-  </tbody>
-  <thead>
-    <tr>
-      <th>M</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>Marvelman</td></tr>
-  </tbody>
-  <thead>
-    <tr>
-      <th>R</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>Robin</td></tr>
-    <tr><td>The Rocketeer</td></tr>
-  </tbody>
-  <thead>
-    <tr>
-      <th>S</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>The Shadow</td></tr>
-    <tr><td>Spider-Man</td></tr>
-    <tr><td>Sub-Mariner</td></tr>
-    <tr><td>Supergirl</td></tr>
-    <tr><td>Superman</td></tr>
-  </tbody>
-  <thead>
-    <tr>
-      <th>T</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>Teenage Mutant Ninja Turtles</td></tr>
-    <tr><td>Thor</td></tr>
-  </tbody>
-  <thead>
-    <tr>
-      <th>W</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>The Wasp</td></tr>
-    <tr><td>Watchmen</td></tr>
-    <tr><td>Wolverine</td></tr>
-    <tr><td>Wonder Woman</td></tr>
-  </tbody>
-  <thead>
-    <tr>
-      <th>X</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>X-Men</td></tr>
-  </tbody>
-  <thead>
-    <tr>
-      <th>Z</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr><td>Zatanna</td></tr>
-    <tr><td>Zatara</td></tr>
-  </tbody>
-  </table>
-</div>
+  <hero title="Nomi Totem"></hero>
 
 
+
+
+
+  <div class="max-w-3xl justify-center container ">
+    <p class="my-8">Il Totem è un nome, di solito di un animale (ma a volte anche di piante, alberi o agenti atmosferici), seguito da un aggettivo, che viene dato alle guide e agli scout per sottolineare una loro caratteristica. Normalmente il nome dell'animale richiama una caratteristica fisica della persona, mentre l'aggettivo una caratteristica della personalità. In passato, nel nostro gruppo, il totem veniva assegnato alle guide del primo anno (prima media) e agli scout dell’ultimo anno (seconda superiore). Oggi il totem viene assegnato in terza media (dal 1991 per gli scout e dal 2006 per le guide), con una cerimonia chiamata “totemizzazione”. Durante questa cerimonia la guida o lo scout per ricevere il suo "Totem" dovrà superare delle prove che testimonieranno il suo valore. </p>
+
+    <div class="form-control w-full ">
+      <label class="label">
+        <span class="label-text">Filtra per nome o per totem</span>
+      </label>
+      <input type="text" v-model="filter" placeholder="Inserisci una parte del nome o del totem" class="input input-bordered w-full " @input="updateTotem()" />
+
+    </div>
+
+
+    <table class="table">
+      <template v-for="(totems, location) in tableTotem">
+        <thead v-if="totems.people.length > 0">
+          <div class="my-12"></div>
+          <tr class="bg-neutral sticky top-20">
+            <th class="rounded-l-xl my-4">{{ location }}</th>
+            <th class="rounded-r-xl">{{ totems.year }}</th>
+          </tr>
+
+        </thead>
+
+
+        <tbody>
+          <tr v-for="person in totems.people" class="hover">
+
+            <td class="rounded-l-xl">{{ person.name }}</td>
+            <td class="rounded-r-xl">{{ person.totem }}</td>
+          </tr>
+        </tbody>
+
+      </template>
+
+    </table>
+
+  </div>
 </template>
+
+
+<script setup>
+
+import { ref, watchEffect } from 'vue'
+
+const totem = ref(null)
+const tableTotem = ref({})
+const filter = ref("")
+
+function updateTotem() {
+
+  tableTotem.value={}
+
+  totem.value.forEach(person => {
+    const location = person['location']
+    if (!tableTotem.value[location]) {
+      tableTotem.value[location] = {
+        'year': person['year'],
+        'people': []
+      }
+    }
+
+    if (filter.value == "" || person.name.toLowerCase().includes(filter.value.toLowerCase()) || person.totem.toLowerCase().includes(filter.value.toLowerCase())) 
+    { tableTotem.value[location].people.push(person) }
+  });
+
+}
+
+watchEffect(() => {
+  fetch('https://people.mirandola2.workers.dev/totem').then(
+    res => res.json()
+  ).then(t => {
+    totem.value = t
+    updateTotem()
+  })
+
+
+})
+
+
+
+</script>
