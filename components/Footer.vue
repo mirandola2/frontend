0a5dcfe1d04ad6beed3e7b74984956b917fd41a6
Giacomo Salici <giambo2000@gmail.com>
2023-08-17 15:44:44 +0200

    minor

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,14 @@
 <template>
+  
     <footer
-      class="footer items-center p-4 bg-neutral text-neutral-content mt-auto max-w-none w-screen"
+      class="mt-auto max-w-none w-screen"
     >
+
+    <div class="mt-20 bg-neutral footer p-4 items-center text-neutral-content w-full">
         <div class="container flex">
         <img src="/logo_colored.png" class="w-16" />
         <p class="my-auto">AGESCI - Gruppo Scout Mirandola 2<br />Via Posta, 55 - 41037 Mirandola
         (MO)<br/><span class="opacity-30">Credit Aquila Ambiziosa</span></p>
-      </div>
+      </div></div>
     </footer>
 </template>
