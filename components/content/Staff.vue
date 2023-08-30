8cf50f48f8d539e8f33d142f4bfa1a088ff70758
Giacomo Salici <giambo2000@gmail.com>
2023-08-30 14:31:44 +0200

    init staff component

---
+++ components/content/Staff.vue
@@ -0,0 +1,24 @@
+<template>
+   <p v-for="capo in capi">{{ capo.nome }} </p>
+
+</template>
+<script setup>
+import { ref } from 'vue'
+const props = defineProps(
+    {
+        staff: {
+            type: String,
+            required: true
+        },
+    }
+)
+
+const capi = ref()
+
+await useAsyncData(() => queryContent('/_capi').findOne().then(res=>{
+    capi.value = res.body.filter((value)=> value.staff == props.staff )
+}))
+
+
+
+</script>
