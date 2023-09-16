66ada3cf04e002cfff3c0e278b2eb48708f91523
Giacomo Salici <giambo2000@gmail.com>
2023-09-16 16:05:41 +0200

    footer minors

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,33 @@
 <template>
-  
-    <footer
-      class="mt-auto max-w-none w-screen"
-    >
+  <footer class="p-10 bg-neutral text-neutral-content">
+    <div class="container footer items-center">
+      <aside class="flex gap-5">
+        <div class="space-y-2">
+          <p>
+          <b>AGESCI - Gruppo Scout Mirandola 2</b><br />Via Posta, 55 - 41037 Mirandola
+          (MO)
+        </p>
 
-    <div class="mt-20 bg-neutral footer p-4 items-center text-neutral-content w-full">
-        <div class="container flex">
-        <img src="/logo_colored.png" class="w-16" />
-        <p class="my-auto">AGESCI - Gruppo Scout Mirandola 2<br />Via Posta, 55 - 41037 Mirandola
-        (MO)<br/><span class="opacity-30">Proudly open-sourced by Baloo & Gaia.</span></p>
-      </div></div>
-    </footer>
+        <p>
+          &copy; {{ new Date().getFullYear() }} - Tutti i diritti riservati
+        </p>
+
+
+        <nav class="text-primary">
+          <div class="grid grid-flow-col gap-4">
+            <a class="link link-hover">Privacy Policy</a>
+            <a class="link link-hover">Contatti</a>
+            <a class="tooltip tooltip-primary link link-hover" data-tip="Sito web open-source by Aquila Ambiziosa e Leoncino Spensierato. Contribuisci su github/mirandola2.">
+              Credit
+            </a>
+          </div>
+        </nav>
+        </div>
+      </aside>
+
+      <p class="text-lg font-serif inline text-primary">
+        <i>Del nostro meglio</i> per <i>essere pronti</i> a <i>servire</i>.
+      </p>
+    </div>
+  </footer>
 </template>
