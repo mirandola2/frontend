7bbada01d9c9c8f697eba16f71371589929f2068
Giacomo Salici <giambo2000@gmail.com>
2023-09-20 15:35:43 +0200

    minor

--- components/Hero.vue
+++ components/Hero.vue
@@ -27,9 +27,9 @@ const bgColor = {
 <template>
   <div class="hero" :style="{'background-image': 'url(' + image + ')'}">
     <div class="hero-overlay bg-opacity-60" :class="bgColor[color]"></div>     
-    <div class="container mx-auto md:px-12 py-20 flex flex-col md:flex-row text-white ">
+    <div class="container max-w-5xl mx-auto md:px-12 py-20 flex flex-col md:flex-row text-white ">
         <img v-if="logo != undefined" :src="logo" class="w-40 pb-8 mx-auto md:p-0 md:w-56 lg:w-64 md:mr-10" />
-        <div class="text-center md:text-left md:mr-auto xl:w-1/2 " :class="{'ml-0': 'logo!=undefined' }">
+        <div class="text-center md:text-left md:mr-auto" :class="{'ml-0': 'logo!=undefined' }">
           <h2 class="text-lg md:text-3xl mb-5 ">AGESCI - Gruppo Scout Mirandola 2</h2>
           <h1 class=" text-5xl md:text-6xl font-display">
             {{ title }}
--- components/content/Staff.vue
+++ components/content/Staff.vue
@@ -1,45 +1,49 @@
 <template>
   <!--parametri capo: nome,nomeCaccia,ruolo,staff,coca,desc,img
 -->
-  <div class="not-prose grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+  <div class="not-prose grid grid-cols-1 md:grid-cols-2 gap-4">
     <div
       v-for="capo in data.body.filter((value) =>
         props.coca == false
-          ? props.staff=='*' || value.staff == props.staff
-          : (props.staff=='*' || value.staff == props.staff) && value.coca == 1
+          ? props.staff == '*' || value.staff == props.staff
+          : (props.staff == '*' || value.staff == props.staff) &&
+            value.coca == 1
       )"
-      class="card bg-base-100"
+      class="card bg-base-200"
     >
       <div class="card-body p-5">
-        <img
-          class="mask mask-squircle w-28 h-28 mx-auto mb-4"
-          :src="
-            capo.img != '' && capo.img != undefined
-              ? capo.img
-              : 'https://api.dicebear.com/7.x/thumbs/svg?seed=' + capo.nome
-          "
-          alt="Avatar Staff"
-        />
+        <div class="flex gap-4 justify-start items-center mb-4">
+          <img
+            class="mask mask-squircle w-24 h-24 "
+            :src="
+              capo.img != '' && capo.img != undefined
+                ? capo.img
+                : 'https://api.dicebear.com/7.x/thumbs/svg?seed=' + capo.nome
+            "
+            alt="Avatar Staff"
+          />
+          <div class="">
+            <div v-if="namesCannotBeShown()">
+              <h2 class="text-xl font-bold">
+                {{ capo.nomeCaccia }}
+              </h2>
+            </div>
+            <div class="align-center pb-0" v-else>
+              <h2 class="text-xl font-bold">{{ capo.nome }}</h2>
+              <p class="">
+                {{ capo.ruolo }}
+              </p>
+            </div>
 
-        
-          <template v-if="namesCannotBeShown()">
-            <h2 class="card-title">
-              {{ capo.nomeCaccia }}
-            </h2>
-           </template
-          >
-          <template v-else
-            ><h2 class="card-title align-center pb-0">
-              {{ capo.nome }}
-            </h2>
-            <p class="align-center justify-">
-              {{ capo.ruolo }}
-              <span class="badge font-bold badge-lg" :class="getColorStaffName(capo.staff)">
-                {{ getFullStaffName(capo.staff) }}
-              </span>
-            </p></template
-          >
-     
+            <div
+              class="badge font-bold badge-lg"
+              v-if="coca"
+              :class="getColorStaffName(capo.staff)"
+            >
+              {{ getFullStaffName(capo.staff) }}
+            </div>
+          </div>
+        </div>
 
         <p class="text-sm" v-if="!coca">{{ capo.desc }}</p>
       </div>
