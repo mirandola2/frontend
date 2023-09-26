51d32301be369c570d728b9e541ea1b4990a6a5e
Giacomo Salici <giambo2000@gmail.com>
2023-09-26 17:14:07 +0200

    daily word

--- pages/index.vue
+++ pages/index.vue
@@ -80,7 +80,7 @@
           </div>
         </div>
 
-        <div class="card max-w-full bg-neutral text-neutral-content divide-base-100">
+        <div class="card max-w-full  bg-neutral text-neutral-content divide-y divide-base-100">
           <div class="card-body">
             <h2 class="card-title">Compleanni</h2>
             <p>
@@ -88,7 +88,7 @@
             </p>
           </div>
           <div class="card-body">
-            <h2 class="card-title">Meteo</h2>
+            <h2 class="card-title">Meteo Mirandola</h2>
             <Weather />
           </div>
           <div class="card-body">
@@ -100,7 +100,7 @@
           <div class="card-body">
             <h2 class="card-title">Liturgia del Giorno</h2>
             <p>
-              <DailyLiturgy />
+              <DailyWord />
             </p>
           </div>
         </div>
