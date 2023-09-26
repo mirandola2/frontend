5564aa672c3fcfeae1e5936d7212c8e858449a2b
Giacomo Salici <giambo2000@gmail.com>
2023-09-26 17:14:07 +0200

    daily word

---
+++ components/DailyWord.vue
@@ -0,0 +1,28 @@
+<template>
+  <span class="loading loading-spinner loading-xs" v-if="!word.loaded"></span>
+  <span v-else>  "{{ word.json.CantoVangelo }}" <a class="link" :href="word.json.Link">({{ word.json.Letture }})</a>
+</span>
+</template>
+<script setup>
+const word = ref({
+  json: "",
+  loaded: false,
+});
+
+
+watchEffect(async () => {
+
+  fetch("https://daily.mirandola2.workers.dev/").then(
+    res => res.json()
+  ).then(
+    json =>{
+      word.value.json = json
+      word.value.loaded = true
+    }
+  )
+  
+});
+
+  
+
+</script>
