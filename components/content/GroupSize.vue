8603fe8d1a45cd13e45199f57661698f0cb66e94
Giacomo Salici <giambo2000@gmail.com>
2023-10-04 18:37:53 +0200

    minor

--- components/content/GroupSize.vue
+++ components/content/GroupSize.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid gap-2 grid-cols-2 lg:grid-cols-4">
+  <div class="grid gap-2 my-5 grid-cols-2 lg:grid-cols-4">
     <div class="stats bg-base-200">
       <div class="stat">
         <div class="stat-title">Totale Soci consiti</div>
--- components/content/TableOfContents.vue
+++ components/content/TableOfContents.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="not-prose card w-96 bg-base-200 text-base-content">
+<div class="not-prose md:float-right md:ml-5 md:mb-5 md:w-96 card w-full bg-base-200 text-base-content">
         <div class="card-body">
             <h2 class="card-title">Indice</h2>
             <p><ul v-if="toc && toc.links">
