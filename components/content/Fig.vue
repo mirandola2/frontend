54d87c294ec8a60796a789cd6ded234396fc73c9
Giacomo Salici <giambo2000@gmail.com>
2023-10-16 23:50:21 +0200

    fig

--- components/content/Fig.vue
+++ components/content/Fig.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="not-prose" :class="{'my-2 max-w-xl': float==0, 'md:float-left m-3 md:ml-0 md:max-w-sm': float==1, 'md:float-right md:max-w-sm m-3 md:mr-0': float==2}">
+  <div class="not-prose" :class="{'my-2 max-w-xl w-full mx-auto': float=='0', 'md:float-left m-3 md:ml-0 md:max-w-sm': float=='1', 'md:float-right md:max-w-sm m-3 md:mr-0': float=='2'}">
     <div class="w-full bg-base-200 p-5 rounded-xl place-items-center ">
       <a :href="src">
-        <img :src="src" :alt="alt || desc" class="rounded-lg shadow-lg m-auto "/>
+        <img :src="src" :alt="alt || desc" class="rounded-lg shadow-lg m-auto max-h-96"/>
       </a>
       <p class="text-sm text-center opacity-70 mt-2 mx-auto">
         {{ desc }}
@@ -27,9 +27,9 @@ const props = defineProps({
     default: "",
   },
   float: {
-    type: Number,
+    type: String,
     required: false,
-    default: 1  //0: not floating, 1: float left, 2: float right
+    default: "2"  //0: not floating, 1: float left, 2: float right
   }
 });
 </script>
