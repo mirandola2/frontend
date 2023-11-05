600363136107bdbf73e8ce0a508800c57f932e06
Giacomo Salici <giambo2000@gmail.com>
2023-11-05 23:47:17 +0100

    better morse

--- components/content/Morse.vue
+++ components/content/Morse.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="max-w-md mx-auto">
+<div class="card not-prose lg:float-right lg:m-5 w-96 bg-neutral">
+  <div class="card-body">
+    <h2 class="card-title">Traduttore Morse</h2>
+    <p>Scrivi un messaggio qui sotto per tradurlo! Puoi anche tradurre direttamente dal codice morse, separando le lettere con <strong>/</strong> e le parole con <strong>//</strong>.</p>
     <textarea
-      class="textarea textarea-lg w-full textarea-bordered bg-base-200 h-24"
-      placeholder="Messaggio"
+      class="textarea w-full textarea-bordered bg-base-200 h-24 font-[monospace]"
+      placeholder="Messaggio (SOS)"
       v-model="mex_text"
       @input="text2morse()"
     ></textarea>
     <textarea
-      class="textarea textarea-lg w-full textarea-bordered bg-base-200 h-24"
-      placeholder="Codice Morse"
+      class="textarea w-full textarea-bordered bg-base-200 h-24 font-[monospace]"
+      placeholder="Codice Morse (... / --- / ...)"
       v-model="mex_morse"
       @input="morse2text()"
     ></textarea>
   </div>
+</div>
+
+
 </template>
 
 <script setup>
@@ -93,38 +99,43 @@ const morseDict = {
   "----.": "9",
   "-----": "0",
   " ": " ",
-  "/": " "
+  "//": " "
 };
 
 function text2morse() {
   mex_text.value = mex_text.value
     .toUpperCase()
-    .replace(/[^A-Z0-9\s\n\r]/g, "")
+    .replace(/[^ A-Z0-9]/g, "")
     .replace(/  +/g, " ");
 
-  var str = [];
-
-  mex_text.value.split("").forEach((letter) => {
-    if (letter == " " || letter == "\r" || letter == "\n") {
-      str.push("/");
-    } else {
+  
+  var morseWords = []
+  mex_text.value.split(" ").forEach((word) => {
+    let str = [];
+    word.split("").forEach((letter)=>{
       str.push(morseDict[letter]);
-    }
+    })
+    morseWords.push(str.join(' / '))
   });
-
-  mex_morse.value = str.join(" ");
+  mex_morse.value = morseWords.join(' // ');
 }
 
 function morse2text() {
   mex_morse.value = mex_morse.value.toUpperCase().replace(/[^ \n./-]/g, "");
-  var str = [];
-
-  mex_morse.value.split(" ").forEach((letter) => {
-    if (letter in morseDict) {
-      str.push(morseDict[letter]);
-    }
+  
+  var textWords = []
+  mex_morse.value.split("//").forEach((word) => {
+    let str = [];
+    word.split('/').forEach((letter)=>{
+      if (letter.trim() in morseDict) {
+      str.push(morseDict[letter.trim()]);
+      }else{
+        str.push('?')
+      }
+    })
+    textWords.push(str.join(""))
   });
 
-  mex_text.value = str.join("");
+  mex_text.value = textWords.join(" ");
 }
 </script>
--- content/morse.md
+++ content/morse.md
@@ -3,10 +3,10 @@ title: Traduttore Morse
 navbar_solid: true 
 show_hero: false
 ---
-
-# Traduttore Morse
-
-Il codice Morse, detto anche alfabeto Morse, è un sistema per trasmettere lettere e numeri e fu uno dei primi metodi di comunicazione a distanza. Un altro metodo per usare il codice morse è con le dita seguendo questa legenda: per esprimere il punto si può usare l’indice, per il trattino si può usare l’indice e il medio. Fu oggetto di studio di Samuel Morse dal 1835 ed è una forma ante litteram di comunicazione digitale.
-
-
 :morse
+
+# Alfabeto Morse
+
+Il [codice Morse](https://it.wikipedia.org/wiki/Codice_Morse), detto anche alfabeto Morse, è un sistema per trasmettere lettere e numeri e fu uno dei primi metodi di comunicazione a distanza. Un altro metodo per usare il codice morse è con le dita seguendo questa legenda: per esprimere il punto si può usare l’indice, per il trattino si può usare l’indice e il medio. Fu oggetto di studio di **Samuel Morse** dal 1835 ed è una forma _ante litteram_ di comunicazione digitale.
+
+
