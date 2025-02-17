be686cf4fdd550d9c7c5aebfd1f50df4f88d0e55
Giacomo Salici <giambo2000@gmail.com>
2025-02-17 22:14:44 +0100

    test ftp 2

--- download.js
+++ download.js
@@ -4,12 +4,13 @@ const fs = require("fs");
 async function downloadImages() {
     const client = new ftp.Client();
     try {
+
         await client.access({
-            host: "ftp.mirandola2.it",
+            host: process.env.FTP_HOST,
             user: process.env.FTP_USER,
             password: process.env.FTP_PW,
         });
-
+        console.log(`Connected to ${process.env.FTP_HOST}`);
         await client.downloadToDir("public/img", "/htdocs/img");
     } catch (err) {
         console.error(err);
